1b975455358d8347dbf27343b38bd76fb1daf94c
cmp 1 <cmp-72@hotmail.com>
2022-07-07 22:38:04 +0200

    Subir cambios en contacto

--- src/pages/ContactoPage.js
+++ src/pages/ContactoPage.js
@@ -1,8 +1,43 @@
 const ContactoPage = (props) => {
     return (
 
-        <div>Contacto</div>
-        
+        <main class="contenedor contacto">
+     <div>
+         <h2>Contacto Rápido</h2>
+         <form action="" method="" class="formulario">
+             <p>
+                 <label for="nombre">Nombre</label>
+                 <input type="text" name=""/>
+             </p>
+             <p>
+                 <label for="email">Email</label>
+                 <input type="email" name=""/>
+             </p>
+             <p>
+                 <label for="number">Telefono</label>
+                 <input type="" name=""/>
+             </p>
+             <p>
+                 <label for="mensaje">Mensaje</label>
+                 <textarea name=""></textarea>
+             </p>
+             <p class="acciones"><input type="submit" value="Enviar"/>
+             </p>
+         </form>
+     </div>
+     <div class="datos">
+         
+         <h2>Otras vías de comunicación</h2>
+         <p> También puede contactar con nosotros a través de:</p>
+         <ul>
+             <li>Telefono: 455 123 456</li>
+             <li>Email: contacto@transportesX.com</li>
+             <li>Facebook:</li>
+             <li>Twitter:</li>
+             <li>Skype:</li>
+         </ul>
+     </div>
+     </main> 
 
         
     );
--- src/pages/GaleriaPage.js
+++ src/pages/GaleriaPage.js
@@ -1,6 +1,17 @@
 const GaleriaPage = (props) => {
     return (
-        <div>Galeria</div>
+        // <div>Galeria</div>
+        <main class="contenedor">
+        <div class="galeria">
+            <img src="img/galeria/ch/img01.jpg" alt=""/>
+            <img src="img/galeria/ch/img02.jpg" alt=""/>
+            <img src="img/galeria/ch/img03.jpg" alt=""/>
+            <img src="img/galeria/ch/img04.jpg" alt=""/>
+            <img src="img/galeria/ch/img05.jpg" alt=""/>
+            <img src="img/galeria/ch/img06.jpg" alt=""/>
+            <img src="img/galeria/ch/img07.jpg" alt=""/>
+        </div> 
+    </main>
     );
 }
 
