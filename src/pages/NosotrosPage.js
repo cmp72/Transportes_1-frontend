f94ad8727986fe51997c06dc7ea3d70e6e4f87ef
cmp 1 <cmp-72@hotmail.com>
2022-07-07 19:01:29 +0200

    Subir nosotros a js

--- src/pages/NosotrosPage.js
+++ src/pages/NosotrosPage.js
@@ -1,7 +1,52 @@
 const NosotrosPage = (props) => {
     return (
 
-        <div>Nosotros</div>
+        // <div>Nosotros</div>
+        <main class="contenedor">
+    <div class="historia">
+        <h2>Historia</h2>
+        <p>xxxxxxxxxxxxxxx</p>
+        <p>sfdvewvgasfdvasdfvmafsd</p>
+    </div>
+    
+    <div class="staff">
+        <h2>Staff</h2>
+        <div class="personas">
+        
+            <div class="persona">
+                <img src="img/nosotros/fotoStaff_1.jpg" alt="Juan Mustafa"/>
+                <h5>Juan Mustafa</h5>
+                <h6>Gerente general</h6>
+                <p>xxxxxxxxxxxxxx</p>
+            </div> 
+            <div class="persona">
+                <img src="img/nosotros/fotoStaff_2.jpg" alt="Cynthia Olivera"/>
+                <h5>Cynthia Olivera</h5>
+                <h6>SubGerente</h6>
+                <p>xxxxxxxxxxxxxx</p>
+            </div> 
+            <div class="persona">
+                <img src="img/nosotros/fotoStaff_3.jpg" alt="Juan Mustafa"/>
+                <h5>Juan Mustafa</h5>
+                <h6>Gerente general</h6>
+                <p>xxxxxxxxxxxxxx</p>
+            </div> 
+            <div class="persona">
+                <img src="img/nosotros/fotoStaff_4.jpg" alt="Juan Mustafa"/>
+                <h5>Juan Mustafa</h5>
+                <h6>Gerente general</h6>
+                <p>xxxxxxxxxxxxxx</p>
+            </div> 
+            <div class="persona">
+                <img src="img/nosotros/fotoStaff_5.jpg" alt="Juan Mustafa"/>
+                <h5>Juan Mustafa</h5>
+                <h6>Gerente general</h6>
+                <p>xxxxxxxxxxxxxx</p>
+            </div> 
+        </div>
+    </div>
+    
+</main>
         
 
         
