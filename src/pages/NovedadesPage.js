fc54c8ea30c42387b1bed8d1f60d0624288a891e
cmp 1 <cmp-72@hotmail.com>
2022-07-08 09:10:05 +0200

    Cambios novedades y servicios

--- src/pages/NovedadesPage.js
+++ src/pages/NovedadesPage.js
@@ -1,10 +1,35 @@
 const NovedadesPage = (props) => {
     return (
+        <main class="contenedor">
+            <h2>Novedades</h2>
+            <div class="novedades">
+                <h3>TITULO</h3>
+                <h4>Subtitulo</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ea id enim</p>
+                <hr />
+            </div>
 
-        <div>Novedades</div>
-        
+            <div class="novedades">
+                <h3>TITULO</h3>
+                <h4>Subtitulo</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ea id enim
+                    voluptate dolores nemo doloremque voluptates aut adipisci commodi.
+                    Cum labore culpa inventore error quisquam aliquid soluta voluptates
+                    explicabo!</p>
+                <hr />
+            </div>
 
-        
+            <div class="novedades">
+                <h3>TITULO</h3>
+                <h4>Subtitulo</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum ea id enim
+                    voluptate dolores nemo doloremque voluptates aut adipisci commodi.
+                    Cum labore culpa inventore error quisquam aliquid soluta voluptates
+                    explicabo!</p>
+                <hr />
+            </div>
+
+        </main>
     );
 }
 
--- src/pages/ServiciosPage.js
+++ src/pages/ServiciosPage.js
@@ -1,7 +1,48 @@
 const ServiciosPage = (props) => {
     return (
 
-        <div>Servicios</div>
+        
+        <main class="contenedor">
+    <h2>Servicios</h2>
+    <div class="servicio">
+        <img src="img/servicios/ferroviario.jpg" alt="Tren"/>
+        <div class="info">
+            <h4>Transporte Ferroviario</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis animi 
+                fugiat officiis cum inventore reprehenderit nulla harum maxime qui, quibusdam, 
+                molestias, neque eligendi alias impedit nam sapiente esse quaerat dolorum?</p>
+        </div>
+    </div>
+    <div class="servicio">
+        <img src="img/servicios/aereo.jpg" alt="Avión"/>
+        <div class="info">
+            <h4>Transporte Aéreo</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+                 placeat itaque omnis! Fuga dolor, molestiae porro exercitationem tempore
+                 quaerat necessitatibus, qui dolorum quisquam 
+                 reprehenderit, ut ab. A libero eum odio.</p>
+        </div>
+    </div>
+    <div class="servicio">
+        <img src="img/servicios/terrestre.jpg" alt="Camión"/>
+        <div class="info">
+            <h4>Transporte Terrestre</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga earum distinctio 
+                voluptates quis consequuntur officiis aliquid? Eligendi ipsa, esse atque dignissimos
+                 modi, minus architecto incidunt mollitia, culpa ipsam cumque pariatur?</p>
+        </div>
+    </div>
+    <div class="servicio">
+        <img src="img/servicios/maritimo.jpg" alt="Barco"/>
+        <div class="info">
+            <h4>Transporte Marítimo</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur 
+                adipisicing elit. Sint cumque, nobis voluptatibus at ex ab accusantium 
+                laudantium harum error dolore, illum minima quam porro deserunt. Pariatur quisquam 
+                vitae iusto ipsum!</p>
+        </div>
+    </div>
+</main>
         
 
         
