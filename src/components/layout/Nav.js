3af88b4aff5653c4522437dbdf63c88777718e3f
cmp 1 <cmp-72@hotmail.com>
2022-07-11 11:30:55 +0200

    final practico

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -15,12 +15,12 @@ const Nav= (props) =>{
             {/* <li><Link to="/contacto">Contacto</Link></li> */}
         {/* </ul> */}
         <ul >
-    <li><NavLink to="/" className={({isActive}) => isActive? "activo" :undifined}>Home</NavLink></li>
-    <li><NavLink to="/nosotros" className={({isActive}) => isActive? "activo" :undifined}>Nosotros</NavLink></li>
-    <li><NavLink to="/servicios" className={({isActive}) => isActive? "activo" :undifined}>Servicios</NavLink></li>
-    <li><NavLink to="/galeria" className={({isActive}) => isActive? "activo" :undifined}>Galeria</NavLink></li>
-    <li><NavLink to="/novedades" className={({isActive}) => isActive? "activo" :undifined}>Novedades</NavLink></li>
-    <li><NavLink to="/contacto" className={({isActive}) => isActive? "activo" :undifined}>Contacto</NavLink></li>
+    <li><NavLink to="/" className={({isActive}) => isActive? "activo" :undefined}>Home</NavLink></li>
+    <li><NavLink to="/nosotros" className={({isActive}) => isActive? "activo" :undefined}>Nosotros</NavLink></li>
+    <li><NavLink to="/servicios" className={({isActive}) => isActive? "activo" :undefined}>Servicios</NavLink></li>
+    <li><NavLink to="/galeria" className={({isActive}) => isActive? "activo" :undefined}>Galeria</NavLink></li>
+    <li><NavLink to="/novedades" className={({isActive}) => isActive? "activo" :undefined}>Novedades</NavLink></li>
+    <li><NavLink to="/contacto" className={({isActive}) => isActive? "activo" :undefined}>Contacto</NavLink></li>
 </ul>
     </div>
 </nav>
